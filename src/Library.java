f0939a6405541529f9d56d9aa83be9dd645d30ca
HoangWorthy <Hoangsusp@gmail.com>
2024-09-29 15:45:43 +0700

    adding addBook and getAllBook function

--- src/Library.java
+++ src/Library.java
@@ -1,2 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Library {
+    private final String[] books = new String[1000];
+
+    public void addBook(String title){
+        books[books.length-1] = title;
+    }
+
+    public void getAllBooks(){
+        for(String title : books){
+            System.out.println(title);
+        }
+    }
 }
